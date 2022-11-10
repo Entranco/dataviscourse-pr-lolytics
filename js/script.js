6a6064342c5247f47e4fb3e86135931701935982
Andrew Fraser <u1171089@utah.edu>
2022-11-09 22:08:38 -0700

    Missed save

--- js/script.js
+++ js/script.js
@@ -2,6 +2,13 @@ years = ['2019', '2020', '2021']
 datMap = {}
 cols = []
 
+function groupDataByAttribute(data) {
+    attrMap = {}
+
+    for ([key, value] in data.entries) {
+        data[key]
+    }
+}
 
 Promise.apply(() => {
     years.forEach(d => {
